6e173db1333866c42eb50b3d366a9b92d334b707
IlijaT <tatalovicilija@gmail.com>
2019-11-11 13:35:16 +0100

    User can filter threads by channel slug

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,13 @@ Route::get('/', function () {
 
 
 
-// Route::resource('threads', 'ThreadsController');
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index')->name('threads.index');
 Route::get('/threads/create', 'ThreadsController@create')->name('threads.create');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show')->name('threads.show');;
+Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::post('/threads', 'ThreadsController@store')->name('threads.store');
 
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
