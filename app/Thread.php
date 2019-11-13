27a9e34caf7481b27aaba33316f67bfc699dea65
IlijaT <tatalovicilija@gmail.com>
2019-11-13 22:25:29 +0100

    Favoritable Trait

--- app/Thread.php
+++ app/Thread.php
@@ -10,6 +10,9 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['creator', 'channel'];
+
+
     protected static function boot()
     {
         parent::boot();
@@ -27,9 +30,7 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     public function creator()
