717e26e64eb6b7698c4578dc200c0c2408197710
IlijaT <tatalovicilija@gmail.com>
2019-11-19 19:52:43 +0100

    Activity Deletion Bug

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Activity;
 use App\Thread;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -89,6 +90,8 @@ class CreateThreadsTest extends TestCase
 
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+
+        $this->assertEquals(0, Activity::count());
     }
 
     public function publishThread($overrides = [])
